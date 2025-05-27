df29072cb28437720ec3f060a59551e8eca67ffe
MuratOzte <mo2553481@gmail.com>
2025-05-27 21:57:52 +0300

    Remove error handling components: delete error.css, error.html, and error.js

--- Client/error massage/error.js
+++
@@ -1,8 +0,0 @@
-function showSnackbar(message) {
-  const snackbar = document.getElementById("snackbar");
-  snackbar.textContent = message;
-  snackbar.className = "show";
-  setTimeout(() => {
-    snackbar.className = snackbar.className.replace("show", "");
-  }, 3000);
-}
